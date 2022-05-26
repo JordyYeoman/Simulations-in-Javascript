846f3bbebf416953a23752a942f3b42600994e5b
Jordy Yeoman <jordyyeoman@gmail.com>
2022-05-26 20:06:25 +0800

    Mo money mo problems

--- simulations.js
+++ simulations.js
@@ -1,7 +1,7 @@
 // Probabilities of event occurring
-let wPercent = 0.1; // If number is equal or between 0 - 10
-let bPercent = 0.8; // If number is equal or between 11 - 89
-let lPercent = 0.1; // If number is equal or between 90 - 100
+let wPercent = 10; // If number is equal or between 0 - 10
+let bPercent = 75; // If number is equal or between 11 - 89
+let lPercent = 15; // If number is equal or between 90 - 100
 let stake = 10; // Amount of money to bet
 let resultsTen = [];
 let resultsTwenty = [];
@@ -15,13 +15,7 @@ const runSimulation = () => {
   let wCount = 0;
   let bCount = 0;
   let lCount = 0;
-  let arrOfSimulationSizes = [
-    // 10,
-    // 20,
-    50,
-    // 100,
-    //1000
-  ];
+  let arrOfSimulationSizes = [10, 20, 50, 100, 1000];
   // Simulate event and if the probability is between the expected value, add to that count
   // 1. Loop over the arrOfSimulationSizes array and grab the value to use as the loop
   arrOfSimulationSizes.forEach((simSize) => {
@@ -29,17 +23,17 @@ const runSimulation = () => {
     while (loopCount < simSize) {
       // Get random value between 0-100
       let randomNumber = Math.floor(Math.random() * 100) + 1;
-      if (randomNumber <= 10) {
+      if (randomNumber <= wPercent) {
         wCount += 1;
-      } else if (randomNumber > 10 && randomNumber < 90) {
+      } else if (randomNumber > wPercent && randomNumber < bPercent) {
         bCount += 1;
-      } else if (randomNumber >= 90) {
+      } else if (randomNumber >= bPercent) {
         lCount += 1;
       }
       loopCount += 1;
     }
     let count = {
-      simSize: simSize,
+      numBets: simSize,
       winCount: wCount,
       bCount: bCount,
       lCount: lCount,
@@ -101,16 +95,16 @@ const runSimulationNTimes = () => {
       loopCount += 1;
     }
     console.log("AMOUNT OF SIMS: " + amountOfSims);
-    // console.log(resultsTen);
-    // console.log(resultsTwenty);
-    // console.log(resultsFifty);
-    // console.log(resultsHundred);
-    // console.log(resultsThousand);
+    console.log(resultsTen);
+    console.log(resultsTwenty);
+    console.log(resultsFifty);
+    console.log(resultsHundred);
+    console.log(resultsThousand);
     var results = {
-      // resultsTen,
-      // resultsTwenty,
+      resultsTen,
+      resultsTwenty,
       resultsFifty,
-      // resultsHundred,
+      resultsHundred,
       amountOfSims,
     };
     averageOf100BetsOverNSimulations.push(results);
@@ -146,6 +140,7 @@ const simulatorBeast = async () => {
   let expectedReturns = [];
 
   averageOf100BetsOverNSimulations.forEach((results) => {
+    console.log("Results: " + JSON.stringify(results));
     let localResults = results.resultsFifty[0];
     // Create expected return
     let winnings = win * localResults.winCount - stake * localResults.winCount;
---
+++ test2.js
@@ -0,0 +1,84 @@
+var xorshift = require('xorshift');
+
+// Test if mug bets will converge on loss if value too different
+
+/// Convert decimal odds to percentage probability
+/// 1/odds * 100
+const getPercentageAsDecimalFromDecimalOdds = (decimalOdds) => {
+  return 1 / decimalOdds;
+};
+
+// Run a simulation 1000 times, where the odds are 1.31 and 3.4.
+// @1.31 odds our stake is $25
+// @3.4 odds our stake is $5
+//
+// What is the outcome of our [bankroll] if we take this bet 1000 times?
+let bankroll = 0;
+const odds1 = 1.31;
+const stake1 = 25;
+// const odds2 = 3.4;
+const stake2 = 5;
+const highProbWinEventProfit = 2.75;
+const lowProbLoseEventLoss = -7;
+
+// Get odds as percentage value represented as decimal
+const oddsOfEvent1 = getPercentageAsDecimalFromDecimalOdds(odds1);
+// const oddsOfEvent2 = getPercentageAsDecimalFromDecimalOdds(odds2);
+const runSimulation = () => {
+  for (let i = 0; i <= 1000; i++) {
+    let randomNumber = getRandomNumber();
+    if (randomNumber >= 0 && randomNumber <= oddsOfEvent1) {
+      bankroll += highProbWinEventProfit;
+      continue;
+    }
+    if (randomNumber <= 1 && randomNumber > oddsOfEvent1) {
+      bankroll += lowProbLoseEventLoss;
+      continue;
+    }
+    console.log('Number didnt fit anywhere!?! WHAT?');
+  }
+};
+
+// Simulate event occuring using probability params
+// If number is between 0 and X probability, then that event occurs
+// If number is between X and 1 probability, then that event occurs
+//
+// Eg - randomInt is 0.25 so event 1 will occur
+// Thanks to below reference for aiding in implementing a more 'true' random number
+// Implemented using xorshift PRNG from - https://github.com/AndreasMadsen/xorshift
+
+// This method returns a random 64-bit double, with its value in the range [0, 1).
+// That means 0 is inclusive and 1 is exclusive. This is equivalent to Math.random().
+const getRandomNumber = () => {
+  var randomNumber = xorshift.random();
+  return randomNumber; // number between 0 and 1
+};
+
+// runSimulation();
+
+// Now that we know [lowProbLoseEventLoss] if it is only double the win, we can turn a profit.
+// Let's get an average simulation the 1000 bets over 10K times
+let arrayOfOutcomes = [];
+
+const rumSimulation10KTimes = () => {
+  for (let k = 0; k <= 10000; k++) {
+    console.log(k);
+    runSimulation();
+    // Push the resulting bankroll to the array of outcomes
+    arrayOfOutcomes.push(bankroll);
+    // Reset bankroll to 0;
+    bankroll = 0;
+  }
+  // Get the average
+  var sum = 0;
+  for (var number of arrayOfOutcomes) {
+    sum += number;
+  }
+  average = sum / arrayOfOutcomes.length;
+  console.log(
+    'Average outcome after running simulation of 1000 bets 10K times: ',
+    average
+  );
+};
+
+rumSimulation10KTimes();
